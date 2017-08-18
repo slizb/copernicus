f0188a262dbd66941ef1363c447230662ca6b92e
slizb <bradsliz@gmail.com>
2017-08-18 00:01:08 -0500

    added initial test suite

--- tests/testthat/test_two_body_system.R
+++ tests/testthat/test_two_body_system.R
@@ -1,6 +1,32 @@
 
+library(copernicus)
 
-
-test_that("hodor is hodor", {
-     expect_match('hodor', "hodor")
+test_that("plot_system() fails given invalid data", {
+     expect_error(plot_system(NA))
+     expect_error(plot_system('asdf'))
+     expect_error(plot_system(123))
 })
+
+test_that("plot_system() returns a plot given valid data", {
+     df <- data.frame(rbind(c(x=1, y=1),
+                            c(x=0, y=0)))
+     df$body <- c('bob', 'martin')
+     a_plot <- plot_system(df)
+
+     expect_is(a_plot, "ggplot")
+
+})
+
+test_that("execute_leapfrog() fails given invalid data", {
+     expect_error(execute_leapfrog())
+     expect_error(execute_leapfrog(NA))
+     expect_error(execute_leapfrog(NULL))
+     expect_error(execute_leapfrog('asdf'))
+})
+
+test_that("execute_leapfrog() works given valid data", {
+     expect_is(execute_leapfrog(1,1,1,1), "list")
+     expect_is(execute_leapfrog(0,2,4,6), "list")
+     expect_is(execute_leapfrog(-10,50,1000,3e6), "list")
+})
+
