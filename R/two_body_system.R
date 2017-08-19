347d5c3b52b72fb876dbec52f39c4db63df91265
slizb <bradsliz@gmail.com>
2017-08-18 23:51:55 -0500

    converted points to images, converted units to astronomical units

--- R/two_body_system.R
+++ R/two_body_system.R
@@ -1,6 +1,9 @@
 
 library(animation)
 library(ggplot2)
+library(dplyr)
+library(png)
+library(grid)
 
 rm(list=ls())
 
@@ -9,7 +12,7 @@ rm(list=ls())
 G = 6.67408e-11     # Newton's gravitational constant (m3 kg-1 s-2)
 M = 1.989e30        # mass of the sun (kg)
 dt = 8.64e4         # time step -one day (seconds)
-
+au = 1.496e11       # astronaumical unit, au (m)
 
 # initializations ----------------------------------------------------
 
@@ -18,9 +21,47 @@ y = 0                    # (m)
 vx_prev_half = 0         # (m/s)
 vy_prev_half = 2.978e4   # (m/s)
 
+# rescale to au's ----------------------------------------------------
+
+G = G / au^3
+x = x / au
+y = y / au
+vx_prev_half = vx_prev_half / au
+vy_prev_half = vy_prev_half / au
+
+# images -------------------------------------------------------------
+
+earth <- readPNG("img/earth.png") %>%
+     rasterGrob(interpolate=FALSE)
+
+sun <- readPNG("img/sun.png") %>%
+     rasterGrob(interpolate=FALSE)
+
+image_dict <- list('earth'=earth,
+                   'sun'=sun)
 
 # functions ----------------------------------------------------------
 
+pad <- function(x, optima, range, size) {
+     buffer <- range * size / 2
+     if (optima == 'max') {
+          x + buffer
+     } else{
+          x - buffer
+     }
+
+}
+
+
+render_body <- function(df, body_name, size) {
+     body_pos <- filter(df, body == body_name)
+
+     annotation_custom(image_dict[[body_name]],
+                       xmin=pad(body_pos$x, 'min', 3, size),
+                       xmax=pad(body_pos$x, 'max', 3, size),
+                       ymin=pad(body_pos$y, 'min', 3, size),
+                       ymax=pad(body_pos$y, 'max', 3, size))
+}
 
 #' Create a static plot of the solar system
 #'
@@ -30,10 +71,10 @@ vy_prev_half = 2.978e4   # (m/s)
 #'
 #' @examples
 plot_system <- function(df) {
-     gg <- ggplot(df, aes(x=x, y=y, color=body ) )
-     points <- geom_point()
-     x_lim <- xlim(-2e11, 2e11)
-     y_lim <- ylim(-2e11, 2e11)
+     qp <- qplot(-1.5:1.5, -1.5:1.5, geom="blank")
+
+     earth_plot <- render_body(df, 'earth', .13)
+     sun_plot <- render_body(df, 'sun', .4)
      opts <- theme(panel.background = element_rect(fill = "black"),
                    panel.grid.major = element_blank(),
                    panel.grid.minor = element_blank(),
@@ -44,7 +85,7 @@ plot_system <- function(df) {
                    axis.title.x=element_blank(),
                    axis.title.y=element_blank())
 
-     return(gg + points + x_lim + y_lim + opts)
+     return(qp + sun_plot + earth_plot + opts)
 }
 
 
@@ -75,6 +116,7 @@ execute_leapfrog <- function(x, y, vx_prev_half, vy_prev_half) {
 # run simulation -----------------------------------------------------
 
 saveGIF(interval = .05,
+        movie.name = "img/2_body_system.gif",
         expr = {
 
              for (i in 1:365){
