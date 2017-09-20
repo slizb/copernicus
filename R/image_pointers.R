4d8315b08b3ad0ba057320baee5f7b197f95624b
Bradley Sliz <bsliz@drivinadmins-MacBook-Pro.local>
2017-09-19 22:10:33 -0500

    initial commit of this script to load planet images

---
+++ R/image_pointers.R
@@ -0,0 +1,32 @@
+
+library(png)
+library(grid)
+
+
+load_image <- function(im_path) {
+  rasterGrob(readPNG(im_path), interpolate=FALSE)
+}
+
+
+sun <- load_image("img/sun.png")
+mercury <- load_image("img/mercury.png")
+venus <- load_image("img/venus.png")
+earth <- load_image("img/earth.png") 
+mars <- load_image("img/mars.png")
+jupiter <- load_image("img/jupiter.png")
+saturn <- load_image("img/saturn.png")
+uranus <- load_image("img/uranus.png")
+neptune <- load_image("img/neptune.png")
+pluto <- load_image("img/pluto.png")
+
+image_dict <- list('sun' = sun,
+                   'mercury' = mercury,
+                   'venus' = venus,
+                   'earth' = earth,
+                   'mars' = mars,
+                   'jupiter' = jupiter,
+                   'saturn' = saturn,
+                   'uranus' = uranus,
+                   'neptune' = neptune,
+                   'pluto' = pluto
+                   )
