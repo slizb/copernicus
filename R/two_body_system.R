f1f66e6194e30b5f10a8fefd9163cc379731cf05
Bradley Sliz <bsliz@drivinadmins-MacBook-Pro.local>
2017-09-19 22:14:02 -0500

    removed constants, initializations, image loads

--- R/two_body_system.R
+++ R/two_body_system.R
@@ -2,43 +2,6 @@
 library(animation)
 library(ggplot2)
 library(dplyr)
-library(png)
-library(grid)
-
-rm(list=ls())
-
-# constants ----------------------------------------------------------
-
-G = 6.67408e-11     # Newton's gravitational constant (m3 kg-1 s-2)
-M = 1.989e30        # mass of the sun (kg)
-dt = 8.64e4         # time step -one day (seconds)
-au = 1.496e11       # astronaumical unit, au (m)
-
-# initializations ----------------------------------------------------
-
-x = 1.496e11             # (m)
-y = 0                    # (m)
-vx_prev_half = 0         # (m/s)
-vy_prev_half = 2.978e4   # (m/s)
-
-# rescale to au's ----------------------------------------------------
-
-G = G / au^3
-x = x / au
-y = y / au
-vx_prev_half = vx_prev_half / au
-vy_prev_half = vy_prev_half / au
-
-# images -------------------------------------------------------------
-
-earth <- readPNG("img/earth.png") %>%
-     rasterGrob(interpolate=FALSE)
-
-sun <- readPNG("img/sun.png") %>%
-     rasterGrob(interpolate=FALSE)
-
-image_dict <- list('earth'=earth,
-                   'sun'=sun)
 
 # functions ----------------------------------------------------------
 
