fe4b2e4abe33d98372f997f552f6b679e75bda1d
Bradley Sliz <bradsliz@gmail.com>
2017-09-20 20:59:11 -0500

    extracted visualization tests into separate script

--- tests/testthat/test_two_body_system.R
+++ tests/testthat/test_two_body_system.R
@@ -1,22 +1,6 @@
 
 library(copernicus)
 
-test_that("plot_system() fails given invalid data", {
-     expect_error(plot_system(NA))
-     expect_error(plot_system('asdf'))
-     expect_error(plot_system(123))
-})
-
-test_that("plot_system() returns a plot given valid data", {
-     df <- data.frame(rbind(c(x=1, y=1),
-                            c(x=0, y=0)))
-     df$body <- c('bob', 'martin')
-     a_plot <- plot_system(df)
-
-     expect_is(a_plot, "ggplot")
-
-})
-
 test_that("execute_leapfrog() fails given invalid data", {
      expect_error(execute_leapfrog())
      expect_error(execute_leapfrog(NA))
---
+++ tests/testthat/test_visualization.R
@@ -0,0 +1,18 @@
+
+library(copernicus)
+
+test_that("plot_system() fails given invalid data", {
+  expect_error(plot_system(NA))
+  expect_error(plot_system('asdf'))
+  expect_error(plot_system(123))
+})
+
+test_that("plot_system() returns a plot given valid data", {
+  df <- data.frame(rbind(c(x=1, y=1),
+                         c(x=0, y=0)))
+  df$body <- c('bob', 'martin')
+  a_plot <- plot_system(df)
+  
+  expect_is(a_plot, "ggplot")
+  
+})
