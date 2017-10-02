f3e9a3daa65ff19c5c2e10ecbb1caed3455b93db
Bradley Sliz <bradsliz@gmail.com>
2017-10-02 16:02:06 -0500

    debugging image relative path  bug

--- R/image_pointers.R
+++ R/image_pointers.R
@@ -1,21 +1,24 @@
 
 
-load_image <- function(im_path) {
+package_root <- rprojroot::find_package_root_file()
+
+load_image <- function(rel_path) {
+  im_path <- paste0(package_root, rel_path)
   grid::rasterGrob(png::readPNG(im_path), interpolate = FALSE)
 }
 
 
 create_image_dict <- function() {
-  sun <- load_image("copernicus/img/sun.png")
-  mercury <- load_image("copernicus/img/mercury.png")
-  venus <- load_image("copernicus/img/venus.png")
-  earth <- load_image("copernicus/img/earth.png") 
-  mars <- load_image("copernicus/img/mars.png")
-  jupiter <- load_image("copernicus/img/jupiter.png")
-  saturn <- load_image("copernicus/img/saturn.png")
-  uranus <- load_image("copernicus/img/uranus.png")
-  neptune <- load_image("copernicus/img/neptune.png")
-  pluto <- load_image("copernicus/img/pluto.png")
+  sun <- load_image("/img/sun.png")
+  mercury <- load_image("/img/mercury.png")
+  venus <- load_image("/img/venus.png")
+  earth <- load_image("/img/earth.png") 
+  mars <- load_image("/img/mars.png")
+  jupiter <- load_image("/img/jupiter.png")
+  saturn <- load_image("/img/saturn.png")
+  uranus <- load_image("/img/uranus.png")
+  neptune <- load_image("/img/neptune.png")
+  pluto <- load_image("/img/pluto.png")
   
   image_dict <- list('sun' = sun,
                      'mercury' = mercury,
