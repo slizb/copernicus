c5a9009ecf2d066014274219a68e2aacde9d3d1a
Bradley Sliz <bradsliz@gmail.com>
2017-10-02 14:32:51 -0500

    debugging travis

--- R/image_pointers.R
+++ R/image_pointers.R
@@ -6,16 +6,16 @@ load_image <- function(im_path) {
 
 
 create_image_dict <- function() {
-  sun <- load_image("img/sun.png")
-  mercury <- load_image("img/mercury.png")
-  venus <- load_image("img/venus.png")
-  earth <- load_image("img/earth.png") 
-  mars <- load_image("img/mars.png")
-  jupiter <- load_image("img/jupiter.png")
-  saturn <- load_image("img/saturn.png")
-  uranus <- load_image("img/uranus.png")
-  neptune <- load_image("img/neptune.png")
-  pluto <- load_image("img/pluto.png")
+  sun <- load_image("../img/sun.png")
+  mercury <- load_image("../img/mercury.png")
+  venus <- load_image("../img/venus.png")
+  earth <- load_image("../img/earth.png") 
+  mars <- load_image("../img/mars.png")
+  jupiter <- load_image("../img/jupiter.png")
+  saturn <- load_image("../img/saturn.png")
+  uranus <- load_image("../img/uranus.png")
+  neptune <- load_image("../img/neptune.png")
+  pluto <- load_image("../img/pluto.png")
   
   image_dict <- list('sun' = sun,
                      'mercury' = mercury,
