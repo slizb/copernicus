dd6f789354bbf8091c9007f989bd1aac7220e712
Bradley Sliz <bradsliz@gmail.com>
2017-10-17 07:07:34 -0500

    updating description of load_image() test

--- tests/testthat/test_image_pointers.R
+++ tests/testthat/test_image_pointers.R
@@ -1,7 +1,7 @@
 
 library(copernicus)
 
-test_that("load_image() returns a png", {
+test_that("load_image() returns a rastergrob object", {
   sun_png = load_image("/img/sun.png")
   expect_is(sun_png, "rastergrob")
 })
