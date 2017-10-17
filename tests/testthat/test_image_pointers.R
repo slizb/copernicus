82fdd6c1fbfea6b7523a6b3246d0792ab4feb51d
Bradley Sliz <bradsliz@gmail.com>
2017-10-17 07:22:40 -0500

    added test for create_image_dict()

--- tests/testthat/test_image_pointers.R
+++ tests/testthat/test_image_pointers.R
@@ -2,6 +2,14 @@
 library(copernicus)
 
 test_that("load_image() returns a rastergrob object", {
-  sun_png = load_image("/img/sun.png")
+  sun_png <- load_image("/img/sun.png")
   expect_is(sun_png, "rastergrob")
+})
+
+test_that("create_image_dict() loads all body images", {
+  image_dict <- create_image_dict()
+  expect_is(image_dict, "list")
+  for (img in image_dict) {
+    expect_is(img, "rastergrob")
+  }
 })
