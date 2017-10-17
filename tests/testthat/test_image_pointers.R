3af3724757d132dd253f64b94025c5ad83d82435
Bradley Sliz <bradsliz@gmail.com>
2017-10-17 16:27:17 -0500

    removed redundant test

--- tests/testthat/test_image_pointers.R
+++ tests/testthat/test_image_pointers.R
@@ -10,7 +10,4 @@ test_that("create_image_dict() loads all body images", {
   image_dict <- create_image_dict()
   expect_is(image_dict, "list")
   expect_length(image_dict, 10)
-  for (img in image_dict) {
-    expect_is(img, "rastergrob")
-  }
 })
