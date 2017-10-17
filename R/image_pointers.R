aacd9fbf81ebb4a280727fdd2380fac5897c0783
Bradley Sliz <bradsliz@gmail.com>
2017-10-17 07:05:27 -0500

    compressing create_image_dict() function

--- R/image_pointers.R
+++ R/image_pointers.R
@@ -1,5 +1,4 @@
 
-
 package_root <- devtools::package_file()
 
 load_image <- function(rel_path) {
@@ -9,26 +8,16 @@ load_image <- function(rel_path) {
 
 
 create_image_dict <- function() {
-  sun <- load_image("/img/sun.png")
-  mercury <- load_image("/img/mercury.png")
-  venus <- load_image("/img/venus.png")
-  earth <- load_image("/img/earth.png") 
-  mars <- load_image("/img/mars.png")
-  jupiter <- load_image("/img/jupiter.png")
-  saturn <- load_image("/img/saturn.png")
-  uranus <- load_image("/img/uranus.png")
-  neptune <- load_image("/img/neptune.png")
-  pluto <- load_image("/img/pluto.png")
   
-  image_dict <- list('sun' = sun,
-                     'mercury' = mercury,
-                     'venus' = venus,
-                     'earth' = earth,
-                     'mars' = mars,
-                     'jupiter' = jupiter,
-                     'saturn' = saturn,
-                     'uranus' = uranus,
-                     'neptune' = neptune,
-                     'pluto' = pluto)
+  image_dict <- list('sun' = load_image("/img/sun.png"),
+                     'mercury' = load_image("/img/mercury.png"),
+                     'venus' = load_image("/img/venus.png"),
+                     'earth' = load_image("/img/earth.png"),
+                     'mars' = load_image("/img/mars.png"),
+                     'jupiter' = load_image("/img/jupiter.png"),
+                     'saturn' = load_image("/img/saturn.png"),
+                     'uranus' = load_image("/img/uranus.png"),
+                     'neptune' = load_image("/img/neptune.png"),
+                     'pluto' = load_image("/img/pluto.png"))
   return(image_dict)
 }
