53dbc2ddef4f8b0de3e43c215586de8bb5d4a622
Bradley Sliz <bradsliz@gmail.com>
2017-11-06 16:31:40 -0600

    added failure test of initialize_system()

--- tests/testthat/test_initializations.R
+++ tests/testthat/test_initializations.R
@@ -2,6 +2,7 @@
 library(copernicus)
 
 test_that("celestial_body() fails given invalid data", {
+  expect_error(celestial_body())
   expect_error(celestial_body(c(0,0), c(0,0) ))
   expect_error(celestial_body(c(0,0), NA, 5))
   expect_error(celestial_body(NULL, c(0,0), 5))
@@ -17,4 +18,11 @@ test_that("solar_system() initializes all of the planets and sun", {
   solar_system <- solar_system()
   expect_is(solar_system, "list")
   expect_length(solar_system, 10)
+})
+
+test_that("initialize_system() fails given invalid data", {
+  expect_error(initialize_system())
+  expect_error(initialize_system(c(0, 2, 1)))
+  expect_error(initialize_system(list(0, 2, 1)) )
+  
 })
