87e2c4cd98cc4b2e8ff16c7f2cb915bf0e72c836
Bradley Sliz <bradsliz@gmail.com>
2017-11-15 16:03:01 -0600

    updating tests of execute_leapfrog() to new argument requirements

--- tests/testthat/test_computation.R
+++ tests/testthat/test_computation.R
@@ -3,14 +3,14 @@ library(copernicus)
 
 test_that("execute_leapfrog() fails given invalid data", {
   expect_error(execute_leapfrog())
-  expect_error(execute_leapfrog(NA, NA, NA, NA, NA))
-  expect_error(execute_leapfrog('a', 'b', 'c', 'd', 'e'))
+  expect_error(execute_leapfrog(NA, NA, NA, NA, NA, NA))
+  expect_error(execute_leapfrog('a', 'b', 'c', 'd', 'e', 'f'))
 })
 
 test_that("execute_leapfrog() works given valid data", {
   constants <- set_constants()
-  expect_is(execute_leapfrog(1,1,1,1, constants), "list")
-  expect_is(execute_leapfrog(0,2,4,6, constants), "list")
-  expect_is(execute_leapfrog(-10,50,1000,3e6, constants), "list")
+  expect_is(execute_leapfrog(1, 1, 1, 1, constants, 1), "list")
+  expect_is(execute_leapfrog(0, 2, 4, 6, constants, 1), "list")
+  expect_is(execute_leapfrog(-10, 50, 1000, 3e6, constants, 1), "list")
 })
 
